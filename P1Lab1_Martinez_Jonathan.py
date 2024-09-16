a3835ff15121ff0055a076f1bb6378c486477f6b
Martinej6236 <martinej6236@student.faytechcc.edu>
2024-09-16 11:24:15 -0400

    First Upload

---
+++ P1Lab1_Martinez_Jonathan.py
@@ -0,0 +1,13 @@
+# Jonathan
+# 9/16/2024
+# P1Lab1
+# Get input from user and display output
+
+# Get first name from the user
+first_name = input("Enter your first name: ")
+
+# Get the Last Name from the user
+last_name = input("Enter your last name: ")
+
+#Display info for the user
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
