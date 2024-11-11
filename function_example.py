a9eb242d97ad6abd2bc791cbc4ca4511ba2c72a6
Martinej6236 <martinej6236@student.faytechcc.edu>
2024-11-11 16:37:02 +0000

    Example

--- function_example.py
+++ function_example.py
@@ -5,22 +5,43 @@ def multiply(num1, num2, num3):
     product = num1 * num2 * num3
     print(product)
 
-
-# define a  function
+# define add  function
 def add(num1, num2, num3):
     result = num1 + num2 + num3
     print(result)
 
+def returnNum():
+    userInput = input("Give me a big number: ")
+    while not userInput.isnumeric():
+       print("Vaule must be a number")
+       userInput = input("Give me a big number: ")
+    return int(userInput)
+
+def getName(lastName):
+    name = input("Enter your first name: ")
+    fullname = "*******" + name + "***************" + lastName
+    return fullname 
+    
+
 # Define Function
-Def main():
+def main():
     # Get input from user
     first_num = int(input("Gimme a number"))
     second_num = int(input("Gimme a number"))
     third_num = int(input("Gimme a number"))
 
+#call the multiply function
+    multiply(first_num, second_num, third_num)
 
 # call the add function
-add(first_num, second_num, third_num)
+    add(first_num, second_num, third_num)
+
+#Call the vaule returning
+    bigNum = returnNum()
+
+    print(bigNum * 5)
+
+    print(getName("Milstead"))
 
 # Call the main function
 if __name__== "__main__":
